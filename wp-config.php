62d96b37c0c43fcf44990564e33192a7be1cc777
Futuhal Arifin Annasri <riff.2192@gmail.com>
2015-06-30 05:23:43 +0700

    remove some lines for security issues

--- wp-config.php
+++ wp-config.php
@@ -23,24 +23,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'p#fi^!Z+UG-@6lqn6LJ`6S2dS/6RO6Ld}-,Ox6m/57]-_04S+vVO,C]hSD&=/Ca*');
-define('SECURE_AUTH_KEY',  '-TN@A*+!p`^soZS[N5Vi,B+9U9-,%3fu[ih*_cY7aP(+(K4=nQ-FIlyi<`0X!yLd');
-define('LOGGED_IN_KEY',    'oSn? |I@Y9*<e($:cwZ)gX,;F}37Hl[S|*+]Jj*?S:L8w!>3HvdN_ui@t*`w.(!%');
-define('NONCE_KEY',        '@jNuNrTH*2)__cUu/]|T]6bW3$G]G#8t2^=o;0:Q2>zzq-E-IePskd2!ga%&-TO0');
-define('AUTH_SALT',        'ccI&p,|c|V?BO;1DO_e]h!y-,YW2HG:w#Q1M~bh<th-Volt3wk-o1Dez3flTO<j(');
-define('SECURE_AUTH_SALT', ']b:>;>2gcQQ=+b]dcd<c+nV.V9mkak;,7u3.Km7;@EuS%`-fB0@q_?_?,dn|PbKG');
-define('LOGGED_IN_SALT',   'v3(BNVM[w@**|2|%#2i[>pSGu#w XB@7/sD(Y&/*)f{Q6f<I:Y `_<FO>kV^{G23');
-define('NONCE_SALT',       '?4>c`v u{`ihgwAiSi,7H54OKEI 49(VkU<.=QfcK&nYP~mx|5q]t=(Dn=i7g3mo');
-
 /**#@-*/
 
 /**
